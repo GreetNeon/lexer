80e659988964a14798a49152b57f9fe3705dd22b
GreetNeon <97176150+GreetNeon@users.noreply.github.com>
2025-04-14 21:59:14 +0000

    Added EatWC function

--- Lexer Files/lexer.c
+++ Lexer Files/lexer.c
@@ -18,16 +18,18 @@ Date Work Commenced:25/2/2025
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include <ctype.h>
 #include "lexer.h"
 
 
 // YOU CAN ADD YOUR OWN FUNCTIONS, DECLARATIONS AND VARIABLES HERE
-
-
-
-
-
+#define NumKeywords 21
+FILE *input;
+int LineCount;
+bool TokenReady;
+Token t;
+const char* keywords[NumKeywords] = {"class", "constructor", "method", "function", "int", "boolean", "char", "void", "var", "static", "field", "let", "do", "if", "else", "while", "return", "true", "false", "null", "this"};
 
 // IMPLEMENT THE FOLLOWING functions
 //***********************************
@@ -37,11 +39,64 @@ Date Work Commenced:25/2/2025
 // This requires opening the file and making any necessary initialisations of the lexer
 // If an error occurs, the function should return 0
 // if everything goes well the function should return 1
+
 int InitLexer (char* file_name)
 {
-  return 0;
+  input = fopen(file_name, "r");
+  if (input == NULL){
+    return 0;
+  }
+  TokenReady = false;
+  LineCount = 1;
+  return 1;
 }
 
+bool IsKeyWord(char* str){
+  for(int i = 0; i < NumKeywords; i++){
+    if(strcmp(str, keywords[i]) == 0){
+      return true;
+    }
+    return false;
+  }
+}
+
+
+int EatWC(){
+  //Consume white space and comments
+  int c = getc(input);
+  while (c != EOF){
+    if (c == '\n'){
+      LineCount++;
+    }
+    if (c == '/'){
+      c = getc(input);
+      if (c == '/'){
+        while (c != '\n' && c != EOF){
+          c = getc(input);
+          
+        }
+        LineCount++;
+      } else if (c == '*'){
+        while (1==1){
+          c = getc(input);
+          if (c == EOF){
+            return 0;
+          } else if (c == '*'){
+            c = getc(input);
+            if (c == '/'){
+              break;
+            }
+          }
+        }
+      } else {
+        return c;
+      }
+    } else if (!isspace(c)){
+      return c;
+    }
+    c = getc(input);
+  }
+}
 
 // Get the next token from the source file
 Token GetNextToken ()
@@ -54,7 +109,9 @@ Token GetNextToken ()
 // peek (look) at the next token in the source file without removing it from the stream
 Token PeekNextToken ()
 {
-  Token t;
+  if (TokenReady){
+    return t;
+  }
   t.tp = ERR;
   return t;
 }
@@ -66,14 +123,26 @@ int StopLexer ()
 }
 
 // do not remove the next line
-#ifndef TEST
+//#ifndef TEST
 int main ()
 {
 	// implement your main function here
   // NOTE: the autograder will not use your main function
-
-  
+  if (IsKeyWord("class")){
+    printf("True\n");
+  }
+  printf("Working\n");
+  if (InitLexer("Main.jack")){
+    printf("File opened successfully\n");
+  } else {
+    printf("File not opened successfully\n");
+  }
+  char first = EatWC();
+  for (int i = 0; i < 5; i++){
+    printf("Next char: %c\n", first);
+    first = EatWC();
+  }
 	return 0;
 }
 // do not remove the next line
-#endif
+//#endif
